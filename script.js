9ab9ecb48049330129717b0fd5d459dcce9a9cb3
example <e@example.com>
2023-06-21 16:07:45 +0300

    version 1.1

--- script.js
+++ script.js
@@ -3,32 +3,50 @@ let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
 addToDoButton.addEventListener('click' , function(){
+    if(inputField.value != ""){
     var paragraph = document.createElement('p');
+    var del = document.createElement('div');
+    del.classList.add('delete-styling');
     paragraph.classList.add('paragraph-styling');
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
-    inputField.value = "";
+    toDoContainer.appendChild(del);
+    inputField.value = "";}
     paragraph.addEventListener('click' , function(){
         paragraph.style.textDecoration = "line-through";
     })
     paragraph.addEventListener('dblclick' , function(){
         paragraph.remove();
+        del.remove();
+    })
+    del.addEventListener('dblclick' , function(){
+        paragraph.remove();
+        del.remove();
     })
 })
 
 inputField.onkeydown = function (event) {
-    if (event.code == 'Enter') 
+    if (event.code == 'Enter' && inputField.value != "") 
     {
         var paragraph = document.createElement('p');
+        var del = document.createElement('div');
+        del.classList.add('delete-styling');
         paragraph.classList.add('paragraph-styling');
         paragraph.innerText = inputField.value;
         toDoContainer.appendChild(paragraph);
+        toDoContainer.appendChild(del);
         inputField.value = "";
+        
     }
     paragraph.addEventListener('click' , function(){
         paragraph.style.textDecoration = "line-through";
     })
     paragraph.addEventListener('dblclick' , function(){
         paragraph.remove();
+        del.remove();
+    })
+    del.addEventListener('dblclick' , function(){
+        paragraph.remove();
+        del.remove();
     })
 }
