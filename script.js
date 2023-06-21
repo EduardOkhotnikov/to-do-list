151b800f1b81f3e370a116cc3784bb72861e9638
example <e@example.com>
2023-06-21 16:35:16 +0300

    version 1.15

--- script.js
+++ script.js
@@ -2,8 +2,7 @@ let addToDoButton = document.getElementById('addToDo');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
-addToDoButton.addEventListener('click' , function(){
-    if(inputField.value != ""){
+function paragraphList () {
     var paragraph = document.createElement('p');
     var del = document.createElement('div');
     del.classList.add('delete-styling');
@@ -11,7 +10,7 @@ addToDoButton.addEventListener('click' , function(){
     paragraph.innerText = inputField.value;
     toDoContainer.appendChild(paragraph);
     toDoContainer.appendChild(del);
-    inputField.value = "";}
+    inputField.value = "";
     paragraph.addEventListener('click' , function(){
         paragraph.style.textDecoration = "line-through";
     })
@@ -23,30 +22,17 @@ addToDoButton.addEventListener('click' , function(){
         paragraph.remove();
         del.remove();
     })
+}
+
+
+addToDoButton.addEventListener('click' , function(){
+    if(inputField.value != ""){
+    paragraphList();}
 })
 
 inputField.onkeydown = function (event) {
     if (event.code == 'Enter' && inputField.value != "") 
     {
-        var paragraph = document.createElement('p');
-        var del = document.createElement('div');
-        del.classList.add('delete-styling');
-        paragraph.classList.add('paragraph-styling');
-        paragraph.innerText = inputField.value;
-        toDoContainer.appendChild(paragraph);
-        toDoContainer.appendChild(del);
-        inputField.value = "";
-        
+        paragraphList();
     }
-    paragraph.addEventListener('click' , function(){
-        paragraph.style.textDecoration = "line-through";
-    })
-    paragraph.addEventListener('dblclick' , function(){
-        paragraph.remove();
-        del.remove();
-    })
-    del.addEventListener('dblclick' , function(){
-        paragraph.remove();
-        del.remove();
-    })
 }
